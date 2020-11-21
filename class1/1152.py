43e24ca5a8313caf9a77465ff89b73295dc221c8
caesiumy <mn0658@naver.com>
2020-11-21 21:04:50 +0900

    Add 20/11/21

---
+++ class1/1152.py
@@ -0,0 +1,7 @@
+# 입력: The Curious Case of Benjamin Button
+# 출력 : 6
+user_input = input()
+print(len(user_input.strip().split()))
+
+# s = input().split()
+# print(len(s))
---
+++ class1/1157.py
@@ -0,0 +1,23 @@
+user_input = input().upper()  
+
+def sol(user_input):
+  obj = {}
+  maxNumber = 0
+  result = ''
+
+  for i in user_input:
+    if i in obj:
+      obj[i] += 1
+    else:
+      obj[i] = 1
+
+  for key in obj:
+    if obj[key] > maxNumber:
+      maxNumber = obj[key]
+      result = key
+    elif obj[key] == maxNumber:
+      result = '?'
+
+  return result
+
+print(sol(user_input))
